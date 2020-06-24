30c1a6ceb2f88288018bf9b341743326bef9c0e7
svasquez <santivasquez1@gmail.com>
2020-06-24 10:30:12 -0500

    Agregar archivos de proyecto.

---
+++ Entidades/RefuerzoLong.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entidades
+{
+    public class RefuerzoLong
+    {
+        public string RefuerzoLongId { get; set; }
+        public Diametro Diametro { get; set; }
+        public float Asi { get; set; }
+        public float[] Coordenadas { get; set; }
+
+        public RefuerzoLong()
+        {
+
+        }
+
+    }
+}
---
+++ Entidades/Story.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entidades
+{
+    public class Story
+    {
+        public string StoryId { get; set; }
+        public string StoryName { get; set; }
+        public float StoryHeight { get; set; }
+        public float StoryElevation { get; set; }
+
+        public Story(string Name, float Height,float elevation)
+        {
+            StoryId = Guid.NewGuid().ToString();
+            StoryName = Name;
+            StoryHeight = Height;
+            StoryElevation = elevation;
+        }        
+
+        public override string ToString()
+        {
+            return $"{StoryName}-{StoryHeight}";
+        }
+
+    }
+}
